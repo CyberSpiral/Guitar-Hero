3f9a6484f10287400ac6643ebef66385a0ab0599
CyberSpiral <CyberSpiral@users.noreply.github.com>
2016-02-02 10:54:54 +0100

    Merge

--- Game/MusicGame/MusicGame/MusicGame/Room.cs
+++ Game/MusicGame/MusicGame/MusicGame/Room.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Content;
 
 namespace MusicGame {
     class Room {
@@ -23,6 +25,31 @@ namespace MusicGame {
         }
         public void Draw() {
 
+        }
+    }
+    class RoomClass {
+        protected Texture2D texture;
+
+        public Rectangle Rectangle { get; set; }
+
+        public static ContentManager Content { get; set; }
+
+        public int roomVersion;
+        public int[] roomVersionDoors;
+
+        public void RoomCreate(int i, Rectangle newRectangle) {
+            texture = Content.Load<Texture2D>("room" + (i));
+            this.Rectangle = newRectangle;
+        }
+
+        public RoomClass(int RoomVersion, int[] RoomVersionDoors) {
+            roomVersion = RoomVersion;
+            roomVersionDoors = RoomVersionDoors;
+        }
+
+        public void Draw(SpriteBatch spriteBatch) {
+            Rectangle = new Rectangle(Rectangle.X, Rectangle.Y, 16, 16);
+            spriteBatch.Draw(texture, Rectangle, Color.White);
         }
     }
 }
