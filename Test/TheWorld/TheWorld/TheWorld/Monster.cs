5552b894a762c826663dbde9de68a94c2d0f71a4
CyberSpiral <CyberSpiral@users.noreply.github.com>
2016-02-04 15:29:58 +0100

    Spit

--- Test/TheWorld/TheWorld/TheWorld/Monster.cs
+++ Test/TheWorld/TheWorld/TheWorld/Monster.cs
@@ -7,8 +7,8 @@ using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace TheWorld {
-    class Monster : GameObject {
-        public Monster(Texture2D texture, Vector2 position, float speed, int textureRows, int textureColumns,
+    class Zombie : GameObject {
+        public Zombie(Texture2D texture, Vector2 position, float speed, int textureRows, int textureColumns,
             int totalFrames, int animationSpeed)
             : base(texture, position, textureRows, textureColumns, totalFrames, animationSpeed) {
             this.speed = speed;
@@ -24,6 +24,33 @@ namespace TheWorld {
             OldPos = Position;
             Position += direction * speed;
 
+        }
+
+
+    }
+    class SpitZombie : GameObject {
+        protected float spitElapsed;
+        public SpitZombie(Texture2D texture, Vector2 position, float speed, int textureRows, int textureColumns,
+            int totalFrames, int animationSpeed)
+            : base(texture, position, textureRows, textureColumns, totalFrames, animationSpeed) {
+            this.speed = speed;
+        }
+
+        public void Update(float elapsed, Vector2 playerPos) {
+            base.Update(elapsed);
+            spitElapsed += elapsed;
+            if (spitElapsed > 100) {
+                Vector2 direction = playerPos - Position;
+                if (direction != Vector2.Zero)
+                    direction.Normalize();
+                rotation = (float)Math.Atan2(direction.Y, direction.X);
+
+                Random r = new Random();
+                direction = new Vector2(r.Next(World.RoomWidth), r.Next(World.RoomHeight))
+
+                OldPos = Position;
+                Position += direction * speed;
+            }
         }
 
 
