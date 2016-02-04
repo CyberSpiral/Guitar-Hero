8e3714cb28ec23823928cdefed9f4e8a3505c93f
ThatTroller <erikove98@gmail.com>
2016-02-04 14:18:36 +0100

    weapon player

---
+++ Test/TheWorld/TheWorld/TheWorld/Player.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace TheWorld
+{
+    class Player : GameObject
+    {
+        public Weapon Weapon { get; set; }
+        protected int storedAnimationSpeed;
+        public Player(Texture2D texture, Vector2 position, float speed, int textureRows, int textureColumns
+            , int totalFrames, int animationSpeed)
+            : base(texture, position, textureRows, textureColumns, totalFrames, animationSpeed)
+        {
+            this.speed = speed;
+            storedAnimationSpeed = animationSpeed;
+        }
+        public void Update(float elapsed, KeyboardState currentKey, KeyboardState oldKey, MouseState mouse)
+        {
+            base.Update(elapsed);
+            Vector2 direction = new Vector2(Mouse.GetState().X, Mouse.GetState().Y) - Position;
+            if (direction != Vector2.Zero)
+                direction.Normalize();
+            rotation = (float)Math.Atan2(direction.Y, direction.X);
+
+            #region CurrentMovement
+            //Can be changed
+            if (currentKey.IsKeyDown(Keys.W))
+                Velocity = new Vector2((float)Math.Cos(rotation), (float)Math.Sin(rotation));
+            if (currentKey.IsKeyDown(Keys.S))
+                Velocity = new Vector2((float)Math.Cos(rotation + MathHelper.ToRadians(180)), (float)Math.Sin(rotation + MathHelper.ToRadians(180)));
+            if (currentKey.IsKeyDown(Keys.D))
+                Velocity = new Vector2((float)Math.Cos(rotation + MathHelper.ToRadians(90)), (float)Math.Sin(rotation + MathHelper.ToRadians(90)));
+            if (currentKey.IsKeyDown(Keys.A))
+                Velocity = new Vector2((float)Math.Cos(rotation + MathHelper.ToRadians(-90)), (float)Math.Sin(rotation + MathHelper.ToRadians(-90)));
+
+
+            if (currentKey.IsKeyDown(Keys.W) && currentKey.IsKeyDown(Keys.D))
+                Velocity = new Vector2((float)Math.Cos(rotation + MathHelper.ToRadians(45)), (float)Math.Sin(rotation + MathHelper.ToRadians(45)));
+            if (currentKey.IsKeyDown(Keys.W) && currentKey.IsKeyDown(Keys.A))
+                Velocity = new Vector2((float)Math.Cos(rotation + MathHelper.ToRadians(-45)), (float)Math.Sin(rotation + MathHelper.ToRadians(-45)));
+            if (currentKey.IsKeyDown(Keys.S) && currentKey.IsKeyDown(Keys.D))
+                Velocity = new Vector2((float)Math.Cos(rotation + MathHelper.ToRadians(135)), (float)Math.Sin(rotation + MathHelper.ToRadians(135)));
+            if (currentKey.IsKeyDown(Keys.S) && currentKey.IsKeyDown(Keys.A))
+                Velocity = new Vector2((float)Math.Cos(rotation + MathHelper.ToRadians(-135)), (float)Math.Sin(rotation + MathHelper.ToRadians(-135)));
+
+            if (currentKey.IsKeyUp(Keys.W) && currentKey.IsKeyUp(Keys.A) && currentKey.IsKeyUp(Keys.S) && currentKey.IsKeyUp(Keys.D))
+            {
+                animationSpeed = int.MaxValue;
+                Velocity = Vector2.Zero;
+            }
+            else {
+                animationSpeed = storedAnimationSpeed;
+            }
+
+            if (Velocity != null)
+                Velocity.Normalize();
+
+            OldPos = Position;
+            Position += Velocity * speed;
+            #endregion
+        }
+    }
+}
---
+++ Test/TheWorld/TheWorld/TheWorld/Weapon.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheWorld
+{
+    class Weapon
+    {
+        public Weapon()
+        {
+
+        }
+
+        public virtual void Execute(Vector2 rotation, Vector2 playerPosition)
+        {
+
+        }
+
+    }
+    class WeaponOnGround : GameObject
+    {
+        public Weapon ContainedWeapon { get; protected set; }
+        public bool existing;
+        public WeaponOnGround(Texture2D texture, Vector2 position, int rows, int columns, int totalFrames
+            , Weapon containedWeapon, int animationSpeed)
+            : base(texture, position, rows, columns, totalFrames, animationSpeed)
+        {
+            ContainedWeapon = containedWeapon;
+            existing = true;
+        }
+
+        public void Draw(SpriteBatch spriteBatch, SpriteFont spriteFont, Vector2 playerPosition)
+        {
+            if (Vector2.Distance(playerPosition, Position) < 70)
+            {
+                spriteBatch.DrawString(spriteFont, "X", new Vector2(0, 0), Color.White);
+            }
+        }
+    }
+}
