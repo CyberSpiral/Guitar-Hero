234efad5b673446f35b3f5e6ebf6ce4618b64bd8
ThatTroller <erikove98@gmail.com>
2016-02-05 09:51:34 +0100

    Doors

--- Test/TheWorld/TheWorld/TheWorld/Gameobject.cs
+++ Test/TheWorld/TheWorld/TheWorld/Gameobject.cs
@@ -6,13 +6,16 @@ using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Graphics;
 
-namespace TheWorld {
-    class GameObject {
+namespace TheWorld
+{
+    class GameObject
+    {
         public Texture2D Texture { get; protected set; }
         public Vector2 Position { get; set; }
         public Vector2 Velocity { get; protected set; }
         public Vector2 OldPos { get; set; }
-        public Rectangle CollisionBox {
+        public Rectangle CollisionBox
+        {
             get { return new Rectangle((int)Position.X - (Texture.Width / Columns / 2), (int)Position.Y - (Texture.Height / Rows / 2), Texture.Width / Columns, Texture.Height / Rows); }
             private set { value = CollisionBox; }
         }
@@ -28,7 +31,8 @@ namespace TheWorld {
         protected float rotation;
         protected float speed;
 
-        public GameObject(Texture2D texture, Vector2 position, int rows, int columns, int totalFrames, int animationSpeed) {
+        public GameObject(Texture2D texture, Vector2 position, int rows, int columns, int totalFrames, int animationSpeed)
+        {
             Texture = texture;
             Position = position;
             totalElapsed = 0;
@@ -38,16 +42,19 @@ namespace TheWorld {
             this.totalFrames = totalFrames;
             this.animationSpeed = animationSpeed;
         }
-        public void Update(float elapsed) {
+        public void Update(float elapsed)
+        {
             totalElapsed += elapsed;
-            if (totalElapsed > animationSpeed) {
+            if (totalElapsed > animationSpeed)
+            {
                 currentFrame++;
                 if (currentFrame == totalFrames)
                     currentFrame = 0;
                 totalElapsed -= animationSpeed;
             }
         }
-        public virtual void Draw(SpriteBatch spriteBatch) {
+        public virtual void Draw(SpriteBatch spriteBatch)
+        {
             int width = Texture.Width / Columns;
             int height = Texture.Height / Rows;
             int row = (int)((float)currentFrame / (float)Columns);
@@ -58,7 +65,8 @@ namespace TheWorld {
 
             spriteBatch.Draw(Texture, destinationRectangle, sourceRectangle, Color.White, rotation - (float)(Math.PI * 0.5f), new Vector2((width / 2), (height / 2)), SpriteEffects.None, 0);
         }
-        public virtual void ChangeTexture(Texture2D texture, int rows, int columns, int totalFrames) {
+        public virtual void ChangeTexture(Texture2D texture, int rows, int columns, int totalFrames)
+        {
             Texture = texture;
 
             Rows = rows;
@@ -68,16 +76,19 @@ namespace TheWorld {
         }
 
     }
-    class Door : GameObject {
+    class Door : GameObject
+    {
 
         private enum Direction : int
         {
             Up, Left, Down, Right
         }
         private Direction direction;
+        private bool active;
 
-        public Door(Texture2D texture, Vector2 position, int rows, int columns, int totalFrames, int animationSpeed) 
-            : base(texture, position, rows, columns, totalFrames, animationSpeed) {
+        public Door(Texture2D texture, Vector2 position, int rows, int columns, int totalFrames, int animationSpeed)
+            : base(texture, position, rows, columns, totalFrames, animationSpeed)
+        {
 
             if (position.X < World.RoomWidth / 16)
             {
@@ -99,7 +110,84 @@ namespace TheWorld {
                 direction = Direction.Down;
                 rotation = (float)Math.PI * 1.5f;
             }
+            ActivateDoors(position);
+        }
 
+        public void ActivateDoors(Vector2 position)
+        {
+            if (position.X < World.RoomWidth / 16)
+            {
+                if (World.ActiveRooms[World.CurrentRoomLocationCode[0]-1,World.CurrentRoomLocationCode[1]])
+                {
+                    active = true;
+                }
+                else
+                {
+                    active = false;
+                }
+            }
+            else if (position.X > World.RoomWidth - World.RoomWidth / 16)
+            {
+                if (World.ActiveRooms[World.CurrentRoomLocationCode[0]+1, World.CurrentRoomLocationCode[1]])
+                {
+                    active = true;
+                }
+                else
+                {
+                    active = false;
+                }
+            }
+            else if (position.Y < World.RoomHeight / 9)
+            {
+                if (World.ActiveRooms[World.CurrentRoomLocationCode[0], World.CurrentRoomLocationCode[1]-1])
+                {
+                    active = true;
+                }
+                else
+                {
+                    active = false;
+                }
+            }
+            else if (position.Y > World.RoomHeight - World.RoomHeight / 9)
+            {
+                if (World.ActiveRooms[World.CurrentRoomLocationCode[0], World.CurrentRoomLocationCode[1]+1])
+                {
+                    active = true;
+                }
+                else
+                {
+                    active = false;
+                }
+            }
+        }
+
+        public void Update(float elapsed, Vector2 playerPos)
+        {
+            base.Update(elapsed);
+            if (active)
+            {
+                if (playerPos.X < World.RoomWidth / 16 || playerPos.X > World.RoomWidth - World.RoomWidth / 16
+                    || playerPos.Y < World.RoomHeight / 9 || playerPos.Y > World.RoomHeight - World.RoomHeight / 9)
+                {
+                    if (direction == Direction.Up)
+                    {
+                        World.CurrentRoomLocationCode[1] -= 1;
+                    }
+                    else if (direction == Direction.Left)
+                    {
+                        World.CurrentRoomLocationCode[0] -= 1;
+                    }
+                    else if (direction == Direction.Down)
+                    {
+                        World.CurrentRoomLocationCode[1] += 1;
+                    }
+                    else if (direction == Direction.Right)
+                    {
+                        World.CurrentRoomLocationCode[0] += 1;
+                    }
+                    ActivateDoors(Position);
+                }
+            }
         }
     }
 }
