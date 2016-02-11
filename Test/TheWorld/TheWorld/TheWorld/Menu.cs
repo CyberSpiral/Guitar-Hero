f51b5ca0c95a292211b65e3ce3b1e881d419c8b5
ThatTroller <erikove98@gmail.com>
2016-02-11 11:23:14 +0100

    MenuWIP

---
+++ Test/TheWorld/TheWorld/TheWorld/Menu.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TheWorld
+{
+    public enum MenuType
+    {
+        StartMenu, PauseMenu, InGame, DeathMenu //add more??
+    }
+
+    class Menu
+    {
+        public MenuType menuType;
+
+        public Menu()
+        {
+            menuType = MenuType.StartMenu;
+        }
+
+        public void Update()
+        {
+            if (menuType == MenuType.StartMenu)
+            {
+                //exit, start
+            }
+            else if (menuType == MenuType.DeathMenu)
+            {
+                ////exit to menu, continue play
+            }
+            else if (menuType == MenuType.PauseMenu)
+            {
+                //exit to menu, continue play
+            }
+            else if (menuType == MenuType.InGame)
+            {
+                //pause button??
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            if (menuType == MenuType.StartMenu)
+            {
+                //exit, start
+            }
+            else if (menuType == MenuType.DeathMenu)
+            {
+                ////exit to menu, continue play
+            }
+            else if (menuType == MenuType.PauseMenu)
+            {
+                //exit to menu, continue play
+            }
+            else if (menuType == MenuType.InGame)
+            {
+                //pause button??
+            }
+        }
+    }
+}
